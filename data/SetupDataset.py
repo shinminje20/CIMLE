ae34a3e519ada398a36d38c1d330ea450424cd38
Minjae Shin <minjaeshin@cs-apex-03s.cmpt.sfu.ca>
2022-09-16 02:32:55 -0700

    updated

--- data/SetupDataset.py
+++ data/SetupDataset.py
@@ -5,6 +5,7 @@ import zipfile
 import gdown
 from tqdm import tqdm
 from DataUtils import *
+import shutil
 
 def gdown_unzip(url, result):
     """Downloads the file at Google drive URL [url], unzips it, and removes any
@@ -21,22 +22,8 @@ def gdown_unzip(url, result):
 
     return f"{data_dir}/{result}"
 
-data2url = {
-    # Data from the Cedar cluster
-    "camnet3": "https://drive.google.com/u/1/uc?id=1Yly8SJitGnA25opej57TRLlSyoGmqeIN&export=download",
-    # Data from https://www.kaggle.com/swaroopkml/cifar10-pngs-in-folders
-    "cifar10": "https://drive.google.com/u/1/uc?id=1_CILJzGysgPLflV2HxXSNMjVzlJat6hs&export=download",
-    # Data from https://mtl.yyliu.net/download/Lmzjm9tX.html
-    "miniImagenet": "https://drive.google.com/u/1/uc?id=15iRcb_h5od0GsTkBGRmVTR8LyfJGuJ2k&export=download",
-    # Data prepared via https://gist.github.com/moskomule/2e6a9a463f50447beca4e64ab4699ac4
-    "tinyImagenet": "https://drive.google.com/u/1/uc?id=1mCQOMcVbN0XT_uwcUjU1gdMvVRpBcX3w&export=download",
-}
-
 if __name__ == "__main__":
     P = argparse.ArgumentParser(description="Dataset downloading and creation")
-    P.add_argument("--data",
-        choices=["tinyImagenet", "miniImagenet", "camnet3", "cifar10", "camnet3_lmdb", "camnet3_deci_lmdb", "camnet3_centi_lmdb"],
-        help="also make a class-first dataset split")
     P.add_argument("--also_cls_first", action="store_true",
         help="also make a class-first dataset split")
     P.add_argument("--sizes", default=[16, 32, 64, 128, 256], type=int,
@@ -46,23 +33,37 @@ if __name__ == "__main__":
         help="use existing dataset instead of downloading one")
     args = P.parse_args()
 
-    if args.use_existing is None:
-        tqdm.write(f"----- Downloading base dataset -----")
-        dataset_dir = gdown_unzip(data2url[args.data], args.data)
-    else:
-        tqdm.write(f"----- Using existing dataset {args.use_existing} -----")
-        args.data = args.use_existing
-        dataset_dir = args.use_existing
+    tqdm.write(f"----- Downloading base dataset -----")
+    dataset_dir = gdown_unzip("https://drive.google.com/u/1/uc?id=1Yly8SJitGnA25opej57TRLlSyoGmqeIN&export=download", "camnet3")
+    data_dir, _ = os.path.split(dataset_dir)
+    new_dataset_dir = data_dir + '/' + "butterfly"
+    
+    try: 
+        shutil.rmtree(new_dataset_dir)
+    except FileNotFoundError:
+        pass
+    os.rename(dataset_dir, new_dataset_dir)
 
+    paths_to_remove = []
+    for d1 in os.listdir(new_dataset_dir):
+        p1 = os.path.join(new_dataset_dir, d1)
+        for i, item in enumerate(os.listdir(p1)):
+            if i != 1:
+                paths_to_remove.append(os.path.join(p1,item))
+
+    for item in paths_to_remove:
+        shutil.rmtree(item)
+
+    
     if len(args.sizes) == 1 and args.sizes[0] <= 0:
         tqdm.write("----- Not resizing dataset -----")
-        all_datasets = [dataset_dir]
+        all_datasets = [new_dataset_dir]
     else:
         tqdm.write(f"----- Generating new resolutions: {args.sizes} -----")
-        all_datasets = [dataset_dir] + [resize_dataset(dataset_dir, s)
+        all_datasets = [new_dataset_dir] + [resize_dataset(new_dataset_dir, s)
             for s in tqdm(args.sizes)]
-
-    if args.also_cls_first and not "lmdb" in args.data:
+        
+    if args.also_cls_first and not "lmdb" in "camnet3":
         tqdm.write(f"----- Making class-first copies -----")
         for dataset in all_datasets:
             make_cls_first(dataset)
